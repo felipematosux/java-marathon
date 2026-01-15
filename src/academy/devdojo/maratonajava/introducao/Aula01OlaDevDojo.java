cef6b1b353d6fd63ffd740ecd6fdfe8b7d072176
Felipe Matos <felipe.silva.94@live.com>
2026-01-15 16:03:07 -0600

    Creation of first exercises about primitive classes and uses.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    // this is a comment in a line
+    /*
+        this is a
+        note with multiple lines
+     */
+
+
+    public static void main(String[] args) {
+        System.out.println("RA-SEN-GAAAAN");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        System.out.println("I am " + age + " years old.");
+        System.out.println(falso);
+
+
+
+    }
+}
