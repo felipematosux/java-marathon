45a995ceeb575e8e0033330d2d3311d97d0e99c5
Felipe Matos <felipe.silva.94@live.com>
2026-01-16 09:31:46 -0600

    Added some lines using casting in primitive types. Forcing some values above the maximum allowed in each type.

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,17 +3,20 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        double salaryDouble = 2000;
-        float salaryFloat = 2500;
-        byte ageByte = 10;
-        short ageShort = 10;
+        int age = (int) 10000000000000L;
+        long bigNumber = (long) 155.23;
+        double salaryDouble = 2000.0D;
+        float salaryFloat = (float) 2500.0D;
+        byte ageByte = 127;
+        short ageShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        char caractere = 'M';
+        char caractere = '\u0041';
 
         System.out.println("I am " + age + " years old.");
         System.out.println(falso);
+        System.out.println("char " + caractere);
+        System.out.println(bigNumber);
 
 
 
