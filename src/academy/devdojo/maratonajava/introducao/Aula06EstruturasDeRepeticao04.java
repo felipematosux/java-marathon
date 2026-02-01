4ed764348d139a9811edc811700b063d91876f9a
Felipe Matos <felipe.silva.94@live.com>
2026-02-01 17:57:09 -0600

    Some more for exercises to fix programming logic

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    // Given the price of a car, calculate how many months you can finance
+    //Condition: monthlyPayment >= 1000
+    public static void main(String[] args) {
+    double maxValue = 30000;
+
+        for (int payment = 1; payment <= maxValue ; payment++) {
+            double monthlyPayment = maxValue / payment;
+            if(monthlyPayment < 1000) {
+                break;
+            }
+            System.out.println("Payment: " + payment + " $" + monthlyPayment);
+        }
+    }
+}
