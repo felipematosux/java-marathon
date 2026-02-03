dc9349d93341f34af2dc8c6e67d33e22a7f5f903
Felipe Matos <felipe.silva.94@live.com>
2026-02-02 22:41:23 -0600

    Created new class with examples of break and continues in if's and for's.

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao05.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao05.java
@@ -1,14 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula06EstruturasDeRepeticao04 {
+public class Aula06EstruturasDeRepeticao05 {
     // Given the price of a car, calculate how many months you can finance
     //Condition: monthlyPayment >= 1000
     public static void main(String[] args) {
         double maxValue = 30000;
-        for (int payment = 1; payment <= maxValue ; payment++) {
+        for (int payment = (int) maxValue; payment >= 1; payment--) {
             double monthlyPayment = maxValue / payment;
-            if(monthlyPayment < 1000) {
-                break;
+            if (monthlyPayment < 1000) {
+                continue;
             }
             System.out.println("Payment: " + payment + " $" + monthlyPayment);
         }
