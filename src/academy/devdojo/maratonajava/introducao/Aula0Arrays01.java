5b1731b36a91abe94d2e5cb2a1677fdd1afc2b2d
Felipe Matos <felipe.silva.94@live.com>
2026-02-03 21:27:42 -0600

    Started the studies about arrays and reference types

---
+++ src/academy/devdojo/maratonajava/introducao/Aula0Arrays01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula0Arrays01 {
+    public static void main(String[] args) {
+        int[] age = new int[3];
+        age[0] = 21;
+        age[1] = 15;
+        age[2] = 11;
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+
+    }
+}
