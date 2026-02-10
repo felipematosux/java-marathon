edfe5d50df0f0803c168e357823ff4ceee66106a
Felipe Matos <felipe.silva.94@live.com>
2026-02-09 21:38:01 -0600

    Started the OOP studies creating new classes packages, test packages e new object

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/StudentTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Student;
+
+public class StudentTest01 {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.name = "Luffy";
+        student.age = 21;
+        student.gender = 'M';
+
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gender);
+
+    }
+}
