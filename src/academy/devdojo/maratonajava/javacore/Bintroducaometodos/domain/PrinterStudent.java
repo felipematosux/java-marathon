eadd614003bcb319b30bd8323844945a3b756212
Felipe Matos <felipe.silva.94@live.com>
2026-02-12 05:19:28 -0600

    methods and reference type parameters in OOP

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/PrinterStudent.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class PrinterStudent {
+    public void print(Student student) {
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gender);
+        System.out.println("-------------------------");
+    }
+}
