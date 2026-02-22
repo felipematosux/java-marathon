8e22bd2137b07db31983b4e5c1e6f21f33d4dc49
Felipe Matos <felipe.silva.94@live.com>
2026-02-22 12:19:02 -0600

    implement method overriding for toString()
    - Overrode the toString() method in the Anime class.
    - Replaced default Object hashcode output with a custom name return.
    - Demonstrated how the print stream interacts with overridden methods.

---
+++ src/academy/devdojo/maratonajava/javacore/Ioverriding/domain/Anime.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Ioverriding.domain;
+
+public class Anime {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public void print(){
+        System.out.println(this.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Ioverriding/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Ioverriding.test;
+
+import academy.devdojo.maratonajava.javacore.Ioverriding.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+}
